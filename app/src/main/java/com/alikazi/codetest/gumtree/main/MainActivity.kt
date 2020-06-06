add60807de86596ab947e4fae3c83d670926f828
mdalikazi <mdalikazi@gmail.com>
2020-06-06 23:10:38 +1000

    Removed unused import

--- app/src/main/java/com/alikazi/codetest/gumtree/main/MainActivity.kt
+++ app/src/main/java/com/alikazi/codetest/gumtree/main/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.alikazi.codetest.gumtree.main
 
 import android.Manifest
-import android.app.Activity
 import android.content.DialogInterface
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
